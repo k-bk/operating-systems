c4ecf2fbfa3d67a900e60d5eb652aefd2aa8c2bb
Karol Bąk <kbak@d17-l428-pc-04.ipa.iisg.agh.edu.pl>
2018-03-16 11:07:48 +0100

    lab works

--- cw02/zad0/iocmp.h
+++ cw02/zad0/iocmp.h
@@ -1,9 +1,9 @@
 #ifndef IOCMP_H
 #define IOCMP_H
 
-extern int genereate (char *name);
-extern int sort (char *name);
-extern int copy (char *name1, char *name2, int record_size);
+extern int genereate (char *name, int recordSize);
+extern int sort (char *name, int recordSize);
+extern int copy (char *name1, char *name2, int recordSize);
 
 #endif //IOCMP_H
 
