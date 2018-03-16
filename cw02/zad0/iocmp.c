9b61c3dad756405f87019a3f71c92848bd6d0751
Karol Bąk <kbak@d17-l428-pc-04.ipa.iisg.agh.edu.pl>
2018-03-16 10:46:43 +0100

    structure of ex2

---
+++ cw02/zad0/iocmp.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "iocmp.h"
+
+int generate (char *name) {
+    FILE *file = fopen(name, w);
+
+    return 0;
+}
+
+int sort (char *name) {
+
+    return 0;
+}
+
+int copy (char *name1, char *name2, int record_size) {
+
+    return 0;
+}
---
+++ cw02/zad0/iocmp.h
@@ -0,0 +1,9 @@
+#ifndef IOCMP_H
+#define IOCMP_H
+
+extern int genereate (char *name);
+extern int sort (char *name);
+extern int copy (char *name1, char *name2, int record_size);
+
+#endif //IOCMP_H
+
