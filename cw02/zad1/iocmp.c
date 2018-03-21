e9712d1ffa67bf160b9d267ae7027bb16d3fac8a
karolBak <bak.karolek@gmail.com>
2018-03-21 20:07:07 +0100

    Split lib and sys functions.

--- cw02/zad1/iocmp.c
+++ cw02/zad1/iocmp.c
@@ -19,61 +19,33 @@ RecordFile newRecordFile (const char *name, int size, int count) {
 
 char outprint[1000000] = {0};
 
-int generate (RecordFile recordFile) {
+void lib_generate (RecordFile recordFile) {
 
-	int randomFile = open ("/dev/random", O_RDONLY);
-	int resultFile = open (recordFile.name, O_WRONLY | O_CREAT);
-	assert (randomFile >= 0 && resultFile >= 0);
-
-	FILE *randomFile = fopen ("/dev/random", "r");
-	FILE *resultFile = fopen (recordFile.name, "w");
-	if (randomFile == NULL || resultFile == NULL) {
-		perror ("Error while opening the file.");
-		return -1;
-    }
-
-    
-    /*
-    CONTENT GENERATION THAT USES /dev/random
-
-    char randInt;
-    for (int i = 0; i < file.count; i++) {
-    if (fread (&randInt, sizeof(randInt), 1, randomFile) != sizeof(randInt)) {
-    for (int i = 0; i < size
-
-    for (int i = 0; i < size; i++) {
-	// add newline or change random int into printable character
-	if ((i + 1) % (recordSize + 1) == 0)
-	    randChar[i] = '\n';
-	else
-	    randChar[i] = (unsigned int) randChar[i] % 50 + 48;
-    }
-    randChar[size - 1] = '\0';
-    fclose (randomFile);
-    */
+    recordFile.file = fopen (recordFile.name, "w");
+    if (recordFile.file == NULL)
+	perror ("Error while opening the file.");
 
     int size = recordFile.count * (recordFile.size + 1);
     char out[recordFile.count][recordFile.size + 1];
     for (int i = 0; i < recordFile.count; i++) {
-		for (int j = 0; j <= recordFile.size; j++) {
-			out[i][j] = rand() % 26 + 65;
-		}
-		out[i][recordFile.size] = '\n';
+	for (int j = 0; j <= recordFile.size; j++) {
+	    out[i][j] = rand() % 26 + 65;
+	}
+	out[i][recordFile.size] = '\n';
     }
 
-	if (sys) {
-		write (resultFile, out, size);
-		close (randomFile);
-		close (resultFile);
-	} else {
-		fwrite (out, 1, size, resultFile);
-		fclose (randomFile);
-		fclose (resultFile);
-	}
-    return 0;
+    if (sys) {
+	write (resultFile, out, size);
+	close (randomFile);
+	close (resultFile);
+    } else {
+	fwrite (out, 1, size, resultFile);
+	fclose (randomFile);
+	fclose (resultFile);
+    }
 }
 
-void print (RecordFile recordFile) {
+void lib_print (RecordFile recordFile) {
     FILE *file = fopen (recordFile.name, "r");
     if (file) {
 		fread (outprint, 1, 100000, file);
@@ -82,33 +54,17 @@ void print (RecordFile recordFile) {
     fclose (file);
 }
 
-int compare (RecordFile recordFile, int i, int j) {
+int lib_compare (RecordFile recordFile, int i, int j) {
     char iVal[1];
     char jVal[1];
-	if (sys) {
-		int file = open (recordFile.name, O_RDWR);
-		lseek (file, i * (recordFile.size + 1), SEEK_SET);
-		read (file, iRecord, sizeof(char) * recordFile.size);
-		lseek (file, j * (recordFile.size + 1), SEEK_SET);
-		read (file, jRecord, sizeof(char) * recordFile.size);
-		lseek (file, i * (recordFile.size + 1), SEEK_SET);
-		write (file, jRecord, recordFile.size);
-		lseek (file, j * (recordFile.size + 1), SEEK_SET);
-		write (file, iRecord, recordFile.size);
-		close (file);
-	} else {
-		FILE *file = fopen (recordFile.name, "r");
-		fseek (file, i * (recordFile.size + 1), 0);
-		fread (iVal, sizeof(char), 1, file);
-		fseek (file, j * (recordFile.size + 1), 0);
-		fread (jVal, sizeof(char), 1, file);
-		fclose (file);
-	}
+    fseek (file, i * (recordFile.size + 1), 0);
+    fread (iVal, sizeof(char), 1, file);
+    fseek (file, j * (recordFile.size + 1), 0);
+    fread (jVal, sizeof(char), 1, file);
     return jVal[0] - iVal[0];
 }
 
-void swap (RecordFile recordFile, int i, int j) {
-    FILE *file = fopen (recordFile.name, "r+");
+void lib_swap (RecordFile recordFile, int i, int j) {
     char iRecord[recordFile.size];
     char jRecord[recordFile.size];
     fseek (file, i * (recordFile.size + 1), 0);
@@ -119,21 +75,25 @@ void swap (RecordFile recordFile, int i, int j) {
     fwrite (jRecord, recordFile.size, 1, file);
     fseek (file, j * (recordFile.size + 1), 0);
     fwrite (iRecord, recordFile.size, 1, file);
-    fclose (file);
 }
 
-int sort (RecordFile recordFile) {
-    for (int i = 1; i < recordFile.count; i++) {
-	int j = i - 1;
-	while (j >= 0 && compare (recordFile, j, j+1) < 0) {
-	    swap (recordFile, j, j+1);
-	    j -= 1;
+void lib_sort (RecordFile recordFile) {
+    recordFile.file = fopen (recordFile.name, "r+");
+    if (recordFile.file != NULL) {
+	/// SORTING   ///////////////////////////////////////////
+	for (int i = 1; i < recordFile.count; i++) {
+	    int j = i - 1;
+	    while (j >= 0 && compare (recordFile, j, j+1) < 0) {
+		swap (recordFile, j, j+1);
+		j -= 1;
+	    }
 	}
+	////////////////////////////////////////////////////////
     }
-    return 0;
+    fclose (recordFile.file);
 }
 
-int copy (RecordFile recordFile1, RecordFile recordFile2, int linesToCopy) {
+void lib_copy (RecordFile recordFile1, RecordFile recordFile2, int linesToCopy) {
     FILE *file1 = fopen (recordFile1.name, "r");
     FILE *file2 = fopen (recordFile2.name, "a");
     assert (file1 && file2);
@@ -148,5 +108,4 @@ int copy (RecordFile recordFile1, RecordFile recordFile2, int linesToCopy) {
     }	    
     fclose (file1);
     fclose (file2);
-    return 0;
 }
--- cw02/zad1/iocmp.h
+++ cw02/zad1/iocmp.h
@@ -5,20 +5,17 @@ typedef struct RecordFile {
     const char *name;
     int size;
     int count;
+    FILE *file;
+    int *fd;
 } RecordFile;
 
 extern RecordFile newRecordFile (const char *name, int size, int count);
-extern int generate (RecordFile recordFile);
-extern int sort (RecordFile recordFile);
-extern int copy (RecordFile recordFile1, RecordFile recordFile2, int linesToCopy);
-int compare (RecordFile recordFile, int i, int j);
-void swap (RecordFile recordFile, int i, int j);
-extern void print (RecordFile recordFile);
-extern int sys_generate (RecordFile recordFile);
-extern int sys_sort (RecordFile recordFile);
-extern int sys_copy (RecordFile recordFile1, RecordFile recordFile2, int linesToCopy);
-int sys_compare (RecordFile recordFile, int i, int j);
-void sys_swap (RecordFile recordFile, int i, int j);
+extern void lib_generate (RecordFile recordFile);
+extern void lib_sort (RecordFile recordFile);
+extern void lib_copy (RecordFile recordFile1, RecordFile recordFile2, int linesToCopy);
+int lib_compare (RecordFile recordFile, int i, int j);
+void lib_swap (RecordFile recordFile, int i, int j);
+void lib_print (RecordFile recordFile);
 
 #endif //IOCMP_H
 
