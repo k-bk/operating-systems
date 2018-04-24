2664802c1a154c8520209b5971cc3ba0c08cf307
karolBak <bak.karolek@gmail.com>
2018-04-24 12:54:19 +0200

    Problems with receiving the message

--- cw06/zad1/client.c
+++ cw06/zad1/client.c
@@ -34,25 +34,29 @@ int main () {
     key_t key = ftok("client.c", rand() % 256);
     int qid = msgget(key, IPC_CREAT | IPC_PRIVATE | S_IRUSR | S_IWUSR);
     if (qid == -1) perror("creating the private FIFO");
-    printf("key: %d\n", key);
+    printf("(client) key: %d\n", key);
+    printf("(client) Waiting for server to connect...\n");
 
     int msgsize = 100;
     size_t buffsize = sizeof(my_msgbuf) + msgsize;
     my_msgbuf* buffer = malloc(buffsize);
+    my_msgbuf* buffer_recieve = malloc(buffsize);
     buffer->type = 1;
     buffer->cmd = START;
     sprintf(buffer->args, "%d", key);
     if (msgsnd(srv_qid, buffer, buffsize, 0) == -1) perror("sending the message");
     msgrcv(qid, buffer, buffsize, 0, 0);
     char command [15];
-    while (1) {
-        printf("Input command: ");
-        scanf("%[A-Z] %[0-9. ]", command, buffer->args);
+    printf("srv_qid: %d qid: %d\n", srv_qid, qid);
+    printf("Welcome to HyperCalc2000. Type [MIRROR|ADD|SUB|MUL|DIV|END] <args>\n> ");
+    while(scanf("%[A-Z] %[0-9. ]", command, buffer->args)) {
         int cmd = command_to_int(command);
-        if (cmd == -1) printf("%s is a wrong command. Type 'help'.\n", command);
+        if (cmd == -1) printf("'%s' is a wrong command. Type 'help'.\n", command);
+        buffer->cmd = cmd;
         if (msgsnd(srv_qid, buffer, buffsize, 0)) perror("sending the command");
-        msgrcv(qid, buffer, buffsize, 0, 0);
-        printf("Result: %s\n", buffer->args);
+        msgrcv(qid, buffer_recieve, buffsize, 0, 0);
+        printf("Result: id: %d args: %s\n", buffer_recieve->id, buffer_recieve->args);
+        printf("> ");
     }
     free(buffer);
     exit (EXIT_SUCCESS);
--- cw06/zad1/server.c
+++ cw06/zad1/server.c
@@ -18,40 +18,47 @@ void free_resources () {
     printf ("ending everything\n");
 }
 
-int my_msgsnd(client client, char *message) {
-    printf("client: %d %d\n", client.id, client.qid);
+int my_msgsnd(client client, char* message) {
     int msglen = strlen(message);
-    my_msgbuf *buffer = malloc(sizeof(my_msgbuf) + msglen);
+    my_msgbuf* buffer = malloc(sizeof(my_msgbuf) + msglen);
     buffer->id = client.id; 
     buffer->type = 1;
+    strcpy(buffer->args, message);
+    printf("(message) id: %d qid: %d args: %s\n", buffer->id, client.qid, buffer->args);
     int status = msgsnd(client.qid, buffer, sizeof(buffer), 0);
     free (buffer);
     return status;
 }
 
 double calculate (int operator, char* args) {
+
     double acc;
     if (operator == ADD || operator == SUB) acc = 0.0;
     else acc = 1.0;
 
-    char* arg;
-    while ((arg = strtok(args, " \n")) != NULL) {
-        switch (operator) {
-            case ADD: acc += atof(arg); break;
-            case SUB: acc -= atof(arg); break;
-            case MUL: acc *= atof(arg); break;
-            case DIV: acc /= atof(arg);
+    char* args_copy = strdup(args);
+    char* arg = NULL;
+    while ((arg = strsep(&args_copy, " \n")) != NULL) {
+        if (strlen(arg) > 0) {
+            printf("(calc) %s\n", arg);
+            switch (operator) {
+                case ADD: acc += atof(arg); break;
+                case SUB: acc -= atof(arg); break;
+                case MUL: acc *= atof(arg); break;
+                case DIV: acc /= atof(arg);
+            }
         }
     }
     return acc;
 }
 
-char *mirror (char *args) {
+char* mirror (char* args) {
     int len = strlen(args);
-    char *result = malloc(len);
+    char* result = malloc(len);
     for (int i = 0; i < len; i++) {
         result[i] = args[len-i-1];
     }
+    printf("(mirror) %s\n", result);
     return result;
 }
 
@@ -61,53 +68,49 @@ void add_client (int id, char* args) {
     if (qid == -1) 
         perror("using given key");
     client_count += 1;
-    clients[client_count].id = id;
-    clients[client_count].qid = qid;
-    printf("sending id: %d to queue: %d\n", id, qid);
-    if (my_msgsnd(clients[client_count], "") == -1) 
+    clients[id].id = id;
+    clients[id].qid = qid;
+    if (my_msgsnd(clients[id], "") == -1) 
         perror("add_client");
-}
-
-client client_with_id (int id) {
-    client client;
-    for (int i = 0; i < client_count; i++) {
-        if (clients[i].id == id) client = clients[i]; 
-    }
-    return client;
 }
 
 int main () {
 
     atexit (free_resources);
     key_t key = ftok ("server.c", 'a');
-    printf("key: %d\n", key);
+    printf("(server) key: %d\n", key);
     int qid = msgget(key, IPC_CREAT | S_IRUSR | S_IWUSR);
     if(qid == -1)
         perror("opening the FIFO");
 
     int id_gen = 0;
     int msglen = 1000;
-    my_msgbuf *buffer = malloc(sizeof(my_msgbuf) + msglen);
+    my_msgbuf* buffer = malloc(sizeof(my_msgbuf) + msglen);
     buffer->type = 1;
 
     while (1) {
         msgrcv(qid, buffer, msglen, 0, 0); 
-        printf("id: %d cmd: %d args: %s\n", buffer->id, buffer->cmd, buffer->args);
+        printf("(recieve) id: %d cmd: %d args: %s\n", buffer->id, buffer->cmd, buffer->args);
+        double result;
         switch (buffer->cmd) {
             case START:
+                printf("(server) Adding client\n");
                 add_client(id_gen++, buffer->args);
                 break;
             case END:
-                printf("Received END signal, aborting...\n");
+                printf("(server) Received END signal, aborting...\n");
                 break;
             case ADD: case SUB: case MUL: case DIV:
-                printf("Sending result");
-                sprintf(buffer->args, "%.2f", calculate(buffer->cmd, buffer->args));
-                my_msgsnd(client_with_id(buffer->id), buffer->args);
+                result = calculate(buffer->cmd, buffer->args);
+                sprintf(buffer->args, "%.2f", result);
+                printf("(message) id: %d qid: %d args: %s\n", buffer->id, clients[buffer->id].qid, buffer->args);
+                if (msgsnd(clients[buffer->id].qid, buffer, msglen, 0) == -1) perror("sending result");
                 break;
             case MIRROR:
-                printf("mirror: %s\n", mirror(buffer->args));
-                my_msgsnd(client_with_id(buffer->id), mirror(buffer->args));
+                my_msgsnd(clients[buffer->id], mirror(buffer->args));
+                break;
+            default:
+                printf("(server) Got wrong message\n");
         }
     }
     exit (EXIT_SUCCESS);
