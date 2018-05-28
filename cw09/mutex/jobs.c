947685ea3101a41d6f92bfc1ea173a911ea6ecc6
karolBak <bak.karolek@gmail.com>
2018-05-28 22:58:44 +0200

    Add functions for cyclic_array

--- cw09/mutex/jobs.c
+++ cw09/mutex/jobs.c
@@ -4,6 +4,14 @@
 #include <errno.h>
 #include <pthread.h>
 
+#define C_RED     "\x1b[31m"
+#define C_GREEN   "\x1b[32m"
+#define C_YELLOW  "\x1b[33m"
+#define C_BLUE    "\x1b[34m"
+#define C_MAGENTA "\x1b[35m"
+#define C_CYAN    "\x1b[36m"
+#define C_RESET   "\x1b[0m"
+
 #define err(x) do { perror(x); exit(EXIT_FAILURE); } while(0)
 #define BUFSIZE 10000000
 #define FILTER_SIZE 100000
@@ -13,8 +21,7 @@ int show_details = 0;
 enum smode_t { GT = 1, LT, EQ };
 enum pmode_t { M_NORMAL, M_MINIMAL };
 
-typedef struct config_t
-{
+typedef struct config_t {
     int P;
     int K;
     int N;
@@ -25,11 +32,83 @@ typedef struct config_t
     int nk;
 } config_t;
 
+typedef struct array_t {
+    int N;
+    char** buffer;
+    int produce;
+    int consume;
+    int elems;
+} array_t;
+
 void clean_up () 
 {
 }
 
-int read_config (config_t* config)
+// ------------ ARRAYS -----------------------------------------
+
+int array_create (array_t* array, int N) {
+    array->buffer = (char**) malloc(N * sizeof(char*));
+    array->produce = 0;
+    array->consume = 0;
+    array->elems = 0;
+    array->N = N;
+    return EXIT_SUCCESS;
+}
+
+int array_add (array_t* array, char* value) 
+{
+    array->buffer[array->produce] = strdup(value);
+    array->produce++;
+    array->produce %= array->N;
+    array->elems++;
+    return EXIT_SUCCESS;
+}
+
+int array_is_full (array_t array) 
+{
+    return array.elems == array.N;
+}
+
+char* array_consume (array_t* array) 
+{
+    if (array->elems <= 0) {
+        return NULL;
+    }
+
+    char* value = strdup(array->buffer[array->consume]);
+    free(array->buffer[array->consume]);
+    array->buffer[array->consume] = NULL;
+    array->consume++;
+    array->consume %= array->N;
+    array->elems--;
+    return value;
+}
+
+void array_print (array_t array) 
+{
+    printf("---\n");
+    for (int i = 0; i < array.N; i++) {
+        if (array.buffer[i] != NULL) {
+            printf(C_YELLOW "[%d]" C_RESET "\t%s\n", i, array.buffer[i]);
+        } else if (i+1 < array.N && array.buffer[i+1] != NULL) {
+            printf("\t...\n");
+        }
+    }
+}
+
+void array_delete (array_t* array)
+{
+    for (int i = 0; i < array->N; i++) {
+        if (array->buffer[i] != NULL) {
+            free(array->buffer[i]);
+        }
+    }
+    free(array->buffer);
+}
+
+// ------------ CONFIG -----------------------------------------
+
+int config_read (config_t* config)
 {
     const int bufsize = 2000;
     char* buffer = malloc(bufsize);
@@ -101,8 +180,11 @@ int read_config (config_t* config)
     return EXIT_SUCCESS;
 }
 
-int test_config (config_t config) {
-    printf( "P = %d K = %d N = %d\n"
+int config_test (config_t config) 
+{
+    printf( "---\n"
+            "config.ini\n"
+            "P = %d K = %d N = %d\n"
             "fname = %s\n"
             "L = %d\n"
             "smode = %d pmode = %d\n"
@@ -112,15 +194,25 @@ int test_config (config_t config) {
     return EXIT_SUCCESS;
 }
 
-void delete_config (config_t config) {
+void config_delete (config_t config) 
+{
     free(config.file_name);
 }
+
+// ------------ MAIN -------------------------------------------
 
 int main (const int argc, const char **argv) 
 {
     config_t config;
-    read_config(&config);
-    test_config(config);
-    delete_config(config);
+    config_read(&config);
+    config_test(config);
+    
+    array_t array;
+    array_create(&array, config.N);
+    array_add(&array, "super linia");
+    array_add(&array, "kolejna linijka");
+
+    config_delete(config);
+    array_delete(&array);
     exit(EXIT_SUCCESS);
 } 
