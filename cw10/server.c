f3787dc924c0099e5f2a909195476b4d0763432f
karolBak <bak.karolek@gmail.com>
2018-06-12 20:42:50 +0200

    Basic messaging working

--- cw10/server.c
+++ cw10/server.c
@@ -4,64 +4,70 @@
 #include <sys/socket.h>
 #include <sys/un.h>
 #include <unistd.h>
+#include <signal.h>
+#include "colors.h"
 
 #define err(msg) do { perror(msg); exit(EXIT_SUCCESS); } while (0);
 
 void doprocessing (int sock);
+void clean_up ();
+
+char socket_path[200];
+
+void clean_up ()
+{
+    unlink(socket_path);
+}
+
+void use_SIGINT (int _)
+{
+    printf(C_RED "Interrupted." C_RESET " Closing...\n");
+    exit(EXIT_SUCCESS);
+}
 
 int main (const int argc, const char** argv)
 {
     if (argc < 3) {
-        printf("Usage: %s <port_number> <socket_path>\n", argv[0]);
+        printf(C_RED "Usage:" C_RESET " %s <port_number> <socket_path>\n", argv[0]);
         exit(EXIT_SUCCESS);
     }
+    atexit(clean_up);
+    signal(SIGINT, use_SIGINT);
 
     const int port_number = atoi(argv[1]);
-    const char* socket_path = argv[2];
-    const int sockfd = socket(AF_UNIX, SOCK_STREAM | SOCK_NONBLOCK, 0);
-    int newsockfd;
+    const int sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
     struct sockaddr_un serv_addr;
     struct sockaddr_un cli_addr;
-    socklen_t clilen = sizeof(cli_addr);
-    int pid;
+    char* message;
 
     serv_addr.sun_family = SOCK_STREAM;
+    strcpy(socket_path, argv[2]);
     strcpy(serv_addr.sun_path, socket_path); 
 
     if (sockfd == -1) {
-        unlink(socket_path);
         err("socket");
     }
     if (bind(sockfd, (struct sockaddr*) &serv_addr, sizeof(serv_addr)) == -1) {
-        unlink(socket_path);
         err("bind");
     }
     if (listen(sockfd, 32) == -1) {
-        unlink(socket_path);
         err("listen");
     }
 
     while (1) {
-        newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, &clilen);
-
-        if (newsockfd < 0)
-            err("accept client");
-
-        pid = fork();
-
-        if (pid < 0)
-            err("fork");
-
-        if (pid == 0) {
+        printf("Waiting for clients...\n");
+        socklen_t clilen = (socklen_t) sizeof(cli_addr);
+        int newsockfd = accept(sockfd, (struct sockaddr*) &cli_addr, &clilen);
+        if (newsockfd < 0) {
             close(sockfd);
-            doprocessing(newsockfd);
-            exit(EXIT_SUCCESS);
-        } else {
-            close(newsockfd);
+            err("accept client");
         }
+        printf("Connection accepted.\n");
+
+        message = "Hello Client, I have got a connection with you.\n";
+        write(newsockfd, message, strlen(message));
     }
     
-    unlink(socket_path);
     exit(EXIT_SUCCESS);
 }
 
